aa591e6e29f2f5c6a7b4e68f6f75d64b24a00804
4knort <4knort@gmail.com>
2016-01-20 03:15:29 +0200

    еще переделал массив

--- js/check.js
+++ js/check.js
@@ -8,16 +8,24 @@ function getMessage(a, b){
     };
   }
   if(typeof(a) == "number"){
-    return("Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + (" аттрибутов"))
+    return("Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " аттрибутов")
   }
-  if(typeof(a) == "object" && typeof(b) == "object"){
-      for(var i = 0; i < a.length; i++){
-        var arr = [];
-        arr.push(a[i] * b[i]);
-        var square = 0;
-        square = square + arr[i];
-        return("Общая площадь артефактов сжатия: " + square +  " пикселей");
-      }
+
+  if(typeof(a) && typeof(b) == "object"){
+    for(var i = 0; i < a.length; i++){
+      var arr = [];
+      arr.push(a[i] * b[i]);
+      var square = 0;
+      square = square + arr[i];
+      return("Общая площадь артефактов сжатия: " + square +  " пикселей");
+    }
+  }
+  if(typeof(a) == "object"){
+    for(var i = 0; i < a.length; i++){
+      var sum = 0;
+      sum = sum + a[i];
+      return("Количество красных точек во всех строчках изображения: " + sum);
+    }
   }
 }
 
