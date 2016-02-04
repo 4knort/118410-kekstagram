27ccad259f9ce3f7c3ea6ff08c7e8873e0befdf3
4knort <4knort@gmail.com>
2016-02-04 12:29:03 +0200

    AJAX

--- js/pictures.js
+++ js/pictures.js
@@ -5,13 +5,62 @@
   var container = document.querySelector('.pictures');
   var template = document.querySelector('#picture-template');
   var filters = document.querySelector('.filters');
+  var pictures = null;
+  var filter = document.querySelectorAll('.filters-item');
+  var fragment = document.createDocumentFragment();
 
-  /*global pictures*/
+  getPictures();
 
-  // перебираем массив с полученными данными из jsonp
-  pictures.forEach(function(picture) {
-    addPicture(picture);
-  });
+  // получаем данные из json
+  function getPictures() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://o0.github.io/assets/json/pictures.json');
+    xhr.onload = function(evt) {
+      var responseData = evt.target.response;
+      pictures = JSON.parse(responseData);
+      renderPictures(pictures);
+      showFilters();
+    };
+
+    xhr.send();
+  }
+
+  //обрабатываем данные полученные из json
+  function renderPictures(data) {
+    // обнуляем содержимое контейнера
+    container.innerHTML = '';
+
+    data.forEach(function(picture) {
+      addPicture(picture);
+    });
+
+    // после обработки всех изображений, запихиваем их разом в контейнер
+    container.appendChild(fragment);
+  }
+
+  //работа с фильтрами
+  for (var i = 0; i <= filter.length; i++) {
+    filter[i].addEventListener('click', function(evt) {
+      var clickedElemId = evt.target.getAttribute('for');
+      setActiveFilter(clickedElemId);
+    });
+  }
+
+  // сортировка изображений в зависимости от активного фильтра
+  function setActiveFilter(id) {
+    var filteredPictures = pictures.slice(0);
+    if (id === 'filter-new') {
+      filteredPictures = filteredPictures.sort(function(a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
+    } else if (id === 'filter-discussed') {
+      filteredPictures = filteredPictures.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+    }
+    renderPictures(filteredPictures);
+  }
+
 
   //функция работы с шаблоном и изобажением
   function addPicture(picture) {
@@ -19,22 +68,26 @@
     var element = template.content.children[0].cloneNode(true);
     element.querySelector('.picture-likes').textContent = picture.likes;
     element.querySelector('.picture-comments').textContent = picture.comments;
-    container.appendChild(element);
+    fragment.appendChild(element);
 
     //изображения
     var imgTag = element.querySelector('img');
     var image = new Image(182, 182);
     var imageLoadTimeout;
 
-    //после загрузи изображения
+    //загрузка изображения
     image.onload = function() {
+      container.classList.add('pictures-loading');
       clearTimeout(imageLoadTimeout);
       element.replaceChild(image, imgTag);
+      container.classList.remove('pictures-loading');
     };
 
     // если произойдет ошибка загрузки изображения
     image.onerror = function() {
+      container.classList.add('pictures-failure');
       element.classList.add('picture-load-failure');
+      container.classList.remove('pictures-failure');
     };
 
     image.src = picture.url;
@@ -48,10 +101,13 @@
 
   }
   //проверяем есть ли в обертке с изображениями какие-то блоки
-  if (container.children.length === 0) {
-    filters.classList.add('hidden');
-  } else {
-    filters.classList.remove('hidden');
+  function showFilters() {
+    if (container.children.length === 0) {
+      filters.classList.add('hidden');
+    } else {
+      filters.classList.remove('hidden');
+    }
   }
+
 
 })();
