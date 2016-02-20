7730c6d2be2381cb694ee192ad8e0da938e26e8b
4knort <4knort@gmail.com>
2016-02-20 18:25:51 +0200

    обработчики в gallery

--- js/gallery.js
+++ js/gallery.js
@@ -6,26 +6,37 @@
     this._closeButton = this.element.querySelector('.gallery-overlay-close');
 
     this._onCloseClick = this._onCloseClick.bind(this);
+    this._onPhotoClick = this._onPhotoClick.bind(this);
+    this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
   };
 
   Gallery.prototype.show = function() {
     this.element.classList.remove('invisible');
 
     this._closeButton.addEventListener('click', this._onCloseClick);
+    document.addEventListener('keydown', this._onDocumentKeyDown);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
     this._closeButton.removeEventListener('click', this._onCloseClick);
+    document.removeEventListener('keydown', this._onDocumentKeyDown);
   };
 
   Gallery.prototype._onCloseClick = function() {
     this.hide();
+  };
+
+  Gallery.prototype._onPhotoClick = function() {
 
   };
 
+  Gallery.prototype._onDocumentKeyDown = function() {
+    if (event.keyCode === 27) {
+      this.element.classList.add('invisible');
+    }
+  };
+
   window.Gallery = Gallery;
-
-
 
 })();
