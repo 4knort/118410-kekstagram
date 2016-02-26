ab3900397e431190eb2dab72a612530056ce3d7d
4knort <4knort@gmail.com>
2016-02-27 01:26:46 +0200

    gallery cont

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,34 @@
 'use strict';
 
 (function() {
-  var Gallery = function() {
+  var Gallery = function(data) {
+    this.data = data;
+    this._onPhotoClick = this._onPhotoClick.bind(this);
+
     this.element = document.querySelector('.gallery-overlay');
     this._closeButton = this.element.querySelector('.gallery-overlay-close');
-
+    this._image = this.element.querySelector('.gallery-overlay-image');
+    this._comments = this.element.querySelector('.comments-count');
+    this._likes = this.element.querySelector('.likes-count');
     this._onCloseClick = this._onCloseClick.bind(this);
     this._onPhotoClick = this._onPhotoClick.bind(this);
     this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+    this.currentPicture = 0;
+  };
+
+  Gallery.prototype.render = function() {
+    this.show();
+    this.setCurrentPicture(this.currentPicture);
+    this._image.addEventListener('click', this._onPhotoClick);
+  }
+  Gallery.prototype.setPictures = function(pictures) {
+    this.pictures = pictures;
+  };
+
+  Gallery.prototype.setCurrentPicture = function(i) {
+    this._image.src = this.pictures[i].url;
+    this._likes.textContent = this.pictures[i].likes;
+    this._comments.textContent = this.pictures[i].comments;
   };
 
   Gallery.prototype.show = function() {
@@ -25,19 +46,29 @@
 
   Gallery.prototype._onCloseClick = function() {
     this.hide();
+    this.removeEventListener('click', _onPhotoClick);
   };
 
   Gallery.prototype._onPhotoClick = function() {
-
+    this.setCurrentPicture(++this.currentPicture);
   };
 
   Gallery.prototype._onDocumentKeyDown = function() {
     if (event.keyCode === 27) {
       this.element.classList.add('invisible');
+      this.removeEventListener('click', _onPhotoClick);
     }
   };
 
-  // inherit(Gallery, Photo);
+  Gallery.prototype.getPictureNumber = function(url) {
+    for (var i = 0; i < this.pictures.length; i++) {
+      if (url === this.pictures[i].url) {
+        this.currentPicture = i;
+        return i;
+      }
+    }
+  };
+
 
   window.Gallery = Gallery;
 
--- js/photo.js
+++ js/photo.js
@@ -4,6 +4,7 @@
 
   function Photo(picture) {
     this._picture = picture;
+    this._onClick = this._onClick.bind(this);
   }
 
   Photo.prototype.render = function() {
@@ -36,7 +37,34 @@
       image.src = '';
       this.element.classList.add('picture-load-failure');
     }.bind(this), IMAGE_TIMEOUT);
+
+    this.element.addEventListener('click', this._onClick);
   };
+
+  Photo.prototype.remove = function() {
+    this.element.removeEventListener('click', this._onClick);
+  }
+  Photo.prototype._onClick = function(evt) {
+    evt.preventDefault();
+    if (this.element.classList.contains('picture') &&
+      !this.element.classList.contains('picture-load-failure')) {
+      if (typeof this.onClick === 'function') {
+        this.onClick();
+      }
+    }
+  }
+
+
+
+  Photo.prototype.onClick = null;
+
+  // Photo.prototype.setData = function(picture) {
+  //   this._picture = picture;
+  // };
+
+  // Photo.prototype.getData = function() {
+  //   return this._picture;
+  // };
 
   window.Photo = Photo;
 })();
